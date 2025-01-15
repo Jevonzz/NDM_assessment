4d3e92535c6fac85e2f6f87fc9a24a31ca807988
Keman <keatvun191@gmail.com>
2025-01-15 20:24:00 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <main>
+      <div>
+        <Header />
+        <Hero />
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex flex-wrap lg:justify-start lg:flex-nowrap z-50 w-full pb-4">
+      <nav className="relative max-w-7xl w-full flex flex-wrap basis-full items-center px-4 md:px-6 lg:px-8 mx-auto">
+        <a
+          className="flex-none rounded-xl text-xl inline-block font-semibold focus:outline-none focus:opacity-80"
+          href="#"
+          aria-label="Casino"
+        >
+          <img
+            src="../../public/casino_logo.png"
+            width={100}
+            height={100}
+            alt="Casino Logo"
+          ></img>
+        </a>
+        <div className="flex items-center gap-x-1 lg:gap-x-2 ms-auto py-1 lg:ps-6 lg:order-3 lg:col-span-3">
+          <button
+            type="button"
+            className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium text-nowrap rounded-xl bg-white border border-gray-200 text-black hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:hover:bg-white/10 dark:text-white dark:hover:text-white dark:focus:text-white"
+          >
+            Sign in
+          </button>
+          <button
+            type="button"
+            className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium text-nowrap rounded-xl border border-transparent bg-red-700 text-white hover:bg-red-500 focus:outline-none focus:bg-red-500 transition disabled:opacity-50 disabled:pointer-events-none"
+          >
+            Log in
+          </button>
+
+          <div className="lg:hidden">
+            <button
+              type="button"
+              className="hs-collapse-toggle size-[38px] flex justify-center items-center text-sm font-semibold rounded-xl border border-gray-200 text-black hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+              id="hs-navbar-hcail-collapse"
+              aria-expanded="false"
+              aria-controls="hs-navbar-hcail"
+              aria-label="Toggle navigation"
+              data-hs-collapse="#hs-navbar-hcail"
+            >
+              <svg
+                className="hs-collapse-open:hidden shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <line x1="3" x2="21" y1="6" y2="6" />
+                <line x1="3" x2="21" y1="12" y2="12" />
+                <line x1="3" x2="21" y1="18" y2="18" />
+              </svg>
+              <svg
+                className="hs-collapse-open:block hidden shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div
+          id="hs-navbar-hcail"
+          className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow lg:block lg:w-auto lg:basis-auto lg:order-2 lg:col-span-6"
+          aria-labelledby="hs-navbar-hcail-collapse"
+        >
+          <div className="flex flex-col gap-y-4 gap-x-0 mt-5 lg:flex-row lg:justify-center lg:items-center lg:gap-y-0 lg:gap-x-7 lg:mt-0">
+            <div>
+              <a
+                className="relative inline-block text-black focus:outline-none dark:text-white hover:border-b-2 border-red-500 hover:animate-bounce"
+                href="#"
+                aria-current="page"
+              >
+                Home
+              </a>
+            </div>
+            <div>
+              <a
+                className="inline-block text-black hover:text-gray-600 focus:outline-none focus:text-gray-600 dark:text-white dark:hover:text-neutral-300 dark:focus:text-neutral-300 hover:border-b-2 border-red-500 hover:animate-bounce"
+                href="#"
+              >
+                Games
+              </a>
+            </div>
+            <div>
+              <a
+                className="inline-block text-black hover:text-gray-600 focus:outline-none focus:text-gray-600 dark:text-white dark:hover:text-neutral-300 dark:focus:text-neutral-300 hover:border-b-2 border-red-500 hover:animate-bounce"
+                href="#"
+              >
+                Bonus
+              </a>
+            </div>
+            <div>
+              <a
+                className="inline-block text-black hover:text-gray-600 focus:outline-none focus:text-gray-600 dark:text-white dark:hover:text-neutral-300 dark:focus:text-neutral-300 hover:border-b-2 border-red-500 hover:animate-bounce"
+                href="#"
+              >
+                Casino News
+              </a>
+            </div>
+            <div>
+              <a
+                className="inline-block text-black hover:text-gray-600 focus:outline-none focus:text-gray-600 dark:text-white dark:hover:text-neutral-300 dark:focus:text-neutral-300 hover:border-b-2 border-red-500 hover:animate-bounce"
+                href="#"
+              >
+                Testimonials
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
